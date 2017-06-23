5b5c4e65e0c310eab8bed819e9a41e8460cd2c53
Ssyndrome <915448715@qq.com>
2017-06-23 15:11:20 +0800

    new way

--- main/main.js
+++ main/main.js
@@ -1,32 +1,55 @@
 "use strict";
-module.exports = function main(input){
-	
-	//最小值
-	var min = input[0];
-	for(var i = 1;i < input.length;i++){
-		if(input[i]<min){
-			min = input[i];
-		}
-	}
-	console.log("o) 最小值 = "+min);
-	
-	//最大值
-	var max = input[0];
-	for(var m = 1;m < input.length;m++){
-		if(input[m]>max){
-			max = input[m];
-		}
-	}
-	console.log("o) 最大值 = "+max);
-	
-	//元素数量
-	console.log("o) 元素数量 =  "+input.length);
-	
-	//平均值
-	var sum = 0;
-	for(var m = 0;m < input.length;m++){
-		sum += input[m];
-	}
-	console.log("o) 平均值 = "+~~(sum/input.length*100)/100);
-	return;
+module.exports = function main(){
+	'use strict'
+function cacl(arr, callback) {
+    var ret;
+    for (var i=0; i<arr.length;i++) {
+        ret = callback(arr[i], ret);
+    }
+    return ret;
+}
+
+Array.prototype.max = function () {
+    return cacl(this, function (item, max) {
+        if (!(max > item)) {
+            return item;
+        }
+        else {
+            return max;
+        }
+    });
+};
+Array.prototype.min = function () {
+    return cacl(this, function (item, min) {
+        if (!(min < item)) {
+            return item;
+        }
+        else {
+            return min;
+        }
+    });
+};
+Array.prototype.sum = function () {
+    return cacl(this, function (item, sum) {
+        if (typeof (sum) == 'undefined') {
+            return item;
+        }
+        else {
+            return sum += item;
+        }
+    }
+	       );
+};
+Array.prototype.avg = function () {
+    if (this.length == 0) {
+        return 0;
+    }
+    return +~~(this.sum(this) / this.length*100)/100;
+};
+
+var theArray=[6, 9, 15, -2, 92, 11];
+console.log(`o) 最小值 = ${theArray.min()}`);
+console.log(`o) 最大值 = ${theArray.max()}`);
+console.log(`o) 元素数量 = ${theArray.length}`);
+console.log(`o) 平均值 = ${theArray.avg()}`);
 };
