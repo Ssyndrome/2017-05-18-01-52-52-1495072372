b5ce6633f9160503e74c5525bcd2629d63b6b486
Ssyndrome <915448715@qq.com>
2017-06-23 14:34:42 +0800

    a stupid method(wish passing~)

--- main/main.js
+++ main/main.js
@@ -1,18 +1,30 @@
-module.exports = function main() {
-    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+module.exports = function main(input){
+	
+	//最小值
+	var min = input[0];
+	for(var i = 1;i < input.length;i++){
+		if(input[i]<min){
+			min = input[i];
+		}
+	}
+	console.log("o) 最小值 = "+min);
+	
+	//最大值
+	var max = input[0];
+	for(var m = 1;m < input.length;m++){
+		if(input[m]>max){
+			max = input[m];
+		}
+	}
+	console.log("o) 最大值 = "+max);
+	
+	//元素数量
+	console.log("o) 元素数量 =  "+input.length);
+	
+	//平均值
+	var sum = 0;
+	for(var m = 0;m < input.length;m++){
+		sum += input[m];
+	}
+	console.log("o) 平均值 = "+~~(sum/input.length*100)/100);
 };
-
-class Sequence {
-  constructor(input) {
-    // Write your code here
-  }
-
-  minimum() {
-    // Write your code here
-  }
-
-  // Write your code here
-}
