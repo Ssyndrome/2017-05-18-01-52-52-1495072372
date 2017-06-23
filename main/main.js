348efcfff6ee1a9ec3f0d62ee6b4797a8f0b7e87
Ssyndrome <915448715@qq.com>
2017-06-23 18:21:22 +0800

    final！

--- main/main.js
+++ main/main.js
@@ -2,10 +2,7 @@
 module.exports = 
 function main() {
     var a = [6, 9, 15, -2, 92, 11];
-    console.log(`o) 最小值 = ${arrMin(a)}`);
-	console.log(`o) 最大值 = ${arrMax(a)}`);
-	console.log(`o) 元素数量 = ${a.length}`);
-	console.log(`o) 平均值 = ${arrAverage(a)}`);
+    console.log(`o) 最小值 = ${arrMin(a)}\n`,`o) 最大值 = ${arrMax(a)}\n`,`o) 元素数量 = ${a.length}\n`,`o) 平均值 = ${arrAverage(a)}`);
 };
 
 
